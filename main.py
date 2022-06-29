52c348d9a44c09a68946d8910464b8ba7bb853df
brian-hornbrook <brian_hornbrook@outlook.com>
2022-06-29 13:07:15 -0500

    added choice functionality for shapes

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
-import turtle
+import turtle as turtle
+import random
 
-t = turtle.Turtle()
+print(
+    "Welcome to your drawing app! Select a shape you want to draw. After typing the number and hitting enter, go to the result tab!")
 
-# draws a hexagon
-for h in ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', ]:
-    t.color(h)
-    t.forward(100)
-    t.right(60)
+turtle.penup()
+turtle.left(90)
+turtle.forward(250)
+turtle.left(90)
+turtle.forward(300)
+turtle.pendown()
 
-# draws a square
-for sq in ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', ]:
-    t.color(sq)
-    t.forward(100)
-    t.right(90)
+turtle.write("Drawing App!", font=("Arial", 16, "normal"))
 
-# draws a star
-for st in ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', ]:
-    t.color(st)
-    t.forward(110)
-    t.right(216)
+turtle.penup()
+turtle.left(90)
+turtle.forward(50)
+turtle.pendown()
+
+turtle.write("To draw a shape, go to the Console tab and choose an option!", font=("Arial", 16, "normal"))
+
+turtle.penup()
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(250)
+turtle.pendown()
+
+
+def star():
+    for st in range(0, 5):
+        turtle.pencolor("blue")
+        turtle.forward(100)
+        turtle.right(144)
+
+
+def square():
+    for sq in range(0, 4):
+        turtle.pencolor("blue")
+        turtle.forward(100)
+        turtle.right(90)
+
+
+def hexagon():
+    for hex in range(0, 6):
+        turtle.pencolor("blue")
+        turtle.forward(100)
+        turtle.right(60)
+
+
+selection = input("1. Star\n2. Square\n3. Hexagon\nSelect a number: ")
+if selection == "1":
+    print("Excellent choice! Go to the result tab to see your creation.")
+    star()
+elif selection == "2":
+    print("Excellent choice! Go to the result tab to see your creation.")
+    square()
+elif selection == "3":
+    print("Excellent choice! Go to the result tab to see your creation.")
+    hexagon()
